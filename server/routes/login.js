c755a1449bb5788a57a5eff5949771b5946bd9f1
Jorge Alexis Paz Barrera <jorgepbarrera38@gmail.com>
2018-05-26 13:38:21 -0500

    asd

--- server/routes/login.js
+++ server/routes/login.js
@@ -9,7 +9,7 @@ const app = express()
 app.post('/login', (req, res) => {
     let body = req.body;
     
-    return body;
+    res.send(body);
     Usuario.findOne({email: body.email}, (err, usuarioDB) => {
         if (err) return res.status(500).json({ok: false, err})
         if (!usuarioDB) return res.status(400).json({ok: false, err: { message: 'Error email' }})
